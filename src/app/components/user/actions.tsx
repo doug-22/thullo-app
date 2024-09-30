8505ed7e87c722ce5c043df6f5a1bc3146fb8fa6
Douglas <douglas.aguiar410@gmail.com>
2024-09-30 11:18:16 -0300

    feat: deploy to firebase hosting

--- src/app/components/user/actions.tsx
+++ src/app/components/user/actions.tsx
@@ -1,11 +1,8 @@
 // "use server"
 
 export async function getUser() {
-  // const response = await fetch('https://api.github.com/users/doug-22')
-  // const data = await response.json()
-  // return data
   await new Promise(resolve => setTimeout(resolve, 3000))
   return {
-    name: "Douglas"
+    name: "Douglas Oliveira"
   }
 }
