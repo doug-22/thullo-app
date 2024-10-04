395dfdc25bd3f97a304f5cffea78451ab568a124
Douglas <douglas.aguiar410@gmail.com>
2024-10-03 22:19:36 -0300

    feat: add tailwind and jotai dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-bg-color': '#F8F9FD'
+      }
+    },
+  },
+  plugins: [],
+}
+
