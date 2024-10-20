969d8b3cb03b84bfa8cb5409764aec9e66113e09
Douglas <douglas.aguiar410@gmail.com>
2024-10-20 11:13:08 -0300

    (feat): create custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,24 @@ module.exports = {
     extend: {
       colors: {
         'custom-bg-color': '#F8F9FD'
+      },
+      spacing: {
+        'screen-dash': 'calc(100vh - 64px)'
+      },
+      keyframes: {
+        'scale-up': {
+          '0%': {
+            transform: 'scale(0.5)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'scale(1)',
+            opacity: '1',
+          },
+        },
+      },
+      animation: {
+        'scale-up': 'scale-up 0.3s ease-in-out',
       }
     },
   },
